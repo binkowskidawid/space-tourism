8c730d021e782292703c24939b0517c0fbbc3bd3
Dawid Binkowski <d.k.binkowski@gmail.com>
2022-12-15 13:18:01 +0100

    Delete imports in destination page.tsx

--- app/(destination)/destination/page.tsx
+++ app/(destination)/destination/page.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
-import DestinationDetails from "../../../components/DestinationDetails";
-import DestinationImage from "../../../components/DestinationImage";
-import DestinationMenu from "../../../components/DestinationMenu";
-import data from "../../../pages/api/data"
-
-export default async  function Destination() {
+export default async function Destination() {
 	return (
 		<main className="container mx-auto min-h-[75vh] mt-16 flex lg:flex-row flex-col justify-between items-center">
-			<div className="lg:h-[70vh] md:w-1/2 flex-1 flex flex-col justify-center items-center md:justify-end md:items-start lg:pl-16">
-			</div>
+			<div className="lg:h-[70vh] md:w-1/2 flex-1 flex flex-col justify-center items-center md:justify-end md:items-start lg:pl-16"></div>
 		</main>
 	);
 }
